593362d580d9de44cb02cfc88be96e7e58e91d86
Wishaal Kanhai <wishaal.kanhai@simplifynow.nl>
2025-05-29 10:14:34 +0200

    Remove shared folder when it exists to prevent crash

--- evo_test_manager.py
+++ evo_test_manager.py
@@ -1,6 +1,7 @@
 """This file contains a class to run and manage evolutionary based testing approaches."""
 import csv
 import glob
+import os
 import shutil
 from concurrent.futures import as_completed
 from concurrent.futures.thread import ThreadPoolExecutor
@@ -217,6 +218,9 @@ class EvoTestManager:
 
     def main(self):
         start_time = datetime.now()
+        dst = "/shared/network"
+        if os.path.exists(dst):
+            shutil.rmtree(dst)
         shutil.copytree("./rocket_interceptor/network", f"/shared/network")
 
 
