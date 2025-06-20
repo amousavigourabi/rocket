fc1c141e775963728f2de4176f4a65208b3d44cd
amousavigourabi <28668597+amousavigourabi@users.noreply.github.com>
2025-06-20 14:52:06 +0200

    Tournament4

--- evo_test_manager.py
+++ evo_test_manager.py
@@ -136,7 +136,7 @@ class EvoTestManager:
         self.image = "rocket-image-atour"
         self.xrpl_image = "ghcr.io/amousavigourabi/docker-rippled/seeded-2.4.0-fully-lowered-threshold:latest"
         # self.output_path = "/data/home/bwassenaar/shared_rocket"
-        self.main_hostname_prefix = "AMG_RouletteN"
+        self.main_hostname_prefix = "AMG_Tournament4N"
         self.shared_volume = f"{self.main_hostname_prefix}_data"
         self.workers = 5  # workers refers to the amount of rocket controllers started at the same time. This means you will need 10 free threads per worker.
         # Do not use more than 5 on the research server!
@@ -298,7 +298,7 @@ class EvoTestManager:
             while len(offspring) < self.population_size:
 
                 # Select two parents using elitism
-                parents = tools.selRoulette(population, 2)
+                parents = tools.selTournament(population, 2, 4)
                 parent1, parent2 = parents[0], parents[1]
 
                 # Clone parents to create children
